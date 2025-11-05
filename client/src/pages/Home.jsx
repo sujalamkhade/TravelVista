2ace854820f29b5cb1a449dc77a2973dcc734610
Sujal Lamkhade <sujalamkhade99@gmail.com>
2025-11-05 16:49:57 +0530

    changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -83,7 +83,7 @@ const Home = () => {
         <div className="backaground_image w-full"></div>
         <div className="top-part w-full gap-2 flex flex-col">
           <h1 className="text-white text-4xl text-center font-bold underline mb-2">
-            The Travel Index[0]
+            Travel Vista
           </h1>
           <h1 className="text-white text-sm text-center xsm:text-lg font-semibold">
             Make Your Travel Dream Come True With Our Amazing Packages
@@ -108,44 +108,8 @@ const Home = () => {
               {/* <FaSearch className="" /> */}
             </button>
           </div>
-          <div className="w-[90%] max-w-xl flex justify-center mt-10">
-            <button
-              onClick={() => {
-                navigate("/search?offer=true");
-              }}
-              className="flex items-center justify-around gap-x-1 bg-slate-400 text-white p-2 py-1 text-[8px] xxsm:text-sm sm:text-lg border-e border-white rounded-s-full flex-1 hover:scale-105 transition-all duration-150"
-            >
-              Best Offers
-              <LuBadgePercent className="text-2xl" />
-            </button>
-            <button
-              onClick={() => {
-                navigate("/search?sort=packageRating");
-              }}
-              className="flex items-center justify-around gap-x-1 bg-slate-400 text-white p-2 py-1 text-[8px] xxsm:text-sm sm:text-lg border-x border-white flex-1 hover:scale-105 transition-all duration-150"
-            >
-              Top Rated
-              <FaStar className="text-2xl" />
-            </button>
-            <button
-              onClick={() => {
-                navigate("/search?sort=createdAt");
-              }}
-              className="flex items-center justify-around gap-x-1 bg-slate-400 text-white p-2 py-1 text-[8px] xxsm:text-sm sm:text-lg border-x border-white flex-1 hover:scale-105 transition-all duration-150"
-            >
-              Latest
-              <FaCalendar className="text-lg" />
-            </button>
-            <button
-              onClick={() => {
-                navigate("/search?sort=packageTotalRatings");
-              }}
-              className="flex items-center justify-around gap-x-1 bg-slate-400 text-white p-2 py-1 text-[8px] xxsm:text-sm sm:text-lg border-s border-white rounded-e-full flex-1 hover:scale-105 transition-all duration-150"
-            >
-              Most Rated
-              <FaRankingStar className="text-2xl" />
-            </button>
-          </div>
+          // Filter //
+         
         </div>
         {/* main page */}
         <div className="main p-6 flex flex-col gap-5">
